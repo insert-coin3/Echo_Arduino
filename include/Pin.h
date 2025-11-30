e28aa6f72bbcd4fb7c3d1df503f1b34df57b4a90
insert-coin3 <yamasaki210827@gmail.com>
2025-11-30 20:53:40 +0900

    pin.h

--- include/Pin.h
+++ include/Pin.h
@@ -13,7 +13,7 @@
 #define PIN_COFFEE_SENSOR      10
 
 #define PIN_ICEDTEA_SERVO      11
-#define PIN_ICEDTEA_LASER     12
+#define PIN_ICEDTEA_LASER      12
 #define PIN_ICEDTEA_SENSOR     13
 
 #define PIN_GREENTEA_SERVO     14
@@ -23,9 +23,11 @@
 #define PIN_WATER_PUMP         4
 #define PIN_WATER_FLOAT_SWITCH 2
 
+#define PIN_DC_MOTOR          17
+
 // ===== 서보 모터 각도 설정 =====
-#define SERVO_ANGLE_CLOSED 0          // 서보 모터 닫힘 각도
-#define SERVO_ANGLE_OPEN 90           // 서보 모터 열림 각도 
+// #define SERVO_ANGLE_CLOSED 0          // 서보 모터 닫힘 각도
+// #define SERVO_ANGLE_OPEN 90           // 서보 모터 열림 각도 
 
 // ===== 타이밍 설정 =====
 #define INTERVAL_SENSOR_READING 1000  // 센서 읽기 주기 (밀리초)
@@ -39,6 +41,7 @@
 #define CMD_PREFIX_COFFEE 'C'
 #define CMD_PREFIX_ICEDTEA 'I'
 #define CMD_PREFIX_GREENTEA 'G'
+#define CMD_PREFIX_DC_MOTOR  'D'
 
 // ===== 재고 상태 문자열 (JSON 값으로 사용) =====
 #define STR_STOCK_HIGH "High"
